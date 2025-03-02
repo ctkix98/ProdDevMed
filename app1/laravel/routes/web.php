0d2392ff0c39f3d8b88ce7054e3332bf5db04b41
Cedrine Tille <cedrine.tille@gmail.com>
2025-03-02 16:08:28 +0100

    Update route

--- app1/laravel/routes/web.php
+++ app1/laravel/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('{n}', function($n) {
+    return "page $n";
+    })->where('n', '[1-3]');
